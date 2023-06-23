37d30dd6157acca5e55b93fc0cc9b9b3cd454be6
VladOS-tech <134133992+VladOS-tech@users.noreply.github.com>
2023-06-24 00:40:38 +0300

    implemented classes group, stream and mark

--- TCPP.cpp
+++ TCPP.cpp
@@ -42,16 +42,6 @@ public:
 	}
 };
 
-class mark {
-	student* student_;
-	int value;
-	string control, lesson;
-public:
-	mark(student* x, int y, string c, string l) {
-		student_ = x; value = y; control = c; lesson = l;
-	}
-};
-
 class att {
 	student* student_;
 	string lesson;
@@ -64,9 +54,9 @@ public:
 	void CreateAtt();
 	friend int Auth();
 };
-void teacher :: CreateMark(student* x, int y, string c, string l) {
-	new mark(x, y, c, l);
-}
+//void teacher :: CreateMark(student* x, int y, string c, string l) {
+//	new mark(x, y, c, l);
+//}
 
 class decan {
 public:
@@ -74,6 +64,55 @@ public:
 	void CheckMarks();
 	void DeleteStudent();
 	friend int Aut();
+};
+
+class group {
+public:
+	bool operator==(const group& other) { return this->name_ == other.name_; }
+	group(string name) { name_ = name; }
+	string name() const noexcept {
+		return name_;
+	}
+	void add_student(student_t student) {
+		if (contains_student(student)) throw runtime_error("Student is already in the group");
+		students_.push_back(student);
+	}
+
+	void remove_student(const shared_ptr<student>& student) {
+		for (auto it = students_.begin(); it != students_.end(); it++) {
+			if (it->get()->mail() == student->mail()) {
+				students_.erase(it);
+				return;
+			}
+		}
+		throw runtime_error("There isn't this student in the group");
+	}
+
+	bool contains_student(const student_t& student) {
+		for (int i = 0; i < students_.size(); i++) {
+			if (students_[i]->mail() == student->mail()) {
+				return true;
+			}
+		}
+		return false;
+	}
+private:
+	vector<student_t> students_;
+	string name_;
+};
+using group_t = shared_ptr<group>;
+
+class task {
+public:
+	enum type_t {
+		LABWORK, TEST, HOMEWORK, PRACTIC, EXAM
+	};
+	task(string name, type_t type) { name_ = name; type_ = type; }
+	type_t type() const noexcept { return type_; }
+	string name() const noexcept { return name_; }
+private:
+	string name_;
+	type_t type_;
 };
 
 class subject {
@@ -93,6 +132,7 @@ public:
 		}
 		throw runtime_error("There isn't this group in the subject");
 	}
+
 	bool contains_group(const shared_ptr<group>& group) {
 		for (int i = 0; i < groups_.size(); i++) {
 			if (*groups_[i] == *group) {
@@ -101,36 +141,70 @@ public:
 		}
 		return false;
 	}
+
+	const vector<shared_ptr<task>>& get_task() const {
+		return tasks_;
+	}
+
+	void add_task(task t) {
+		tasks_.push_back(make_shared<task>(t));
+	}
 private:
 	string name_;
 	teacher* teacher_;
 	vector<shared_ptr<group>> groups_;
+	vector<shared_ptr<task>> tasks_;
 };
 
-class task {
+class mark {
+	student_t student_;
+	int value_;
+	task* task_;
 public:
-	enum type_t {
-		LABWORK, TEST, HOMEWORK, PRACTIC, EXAM  
-	};
-	task(string name, type_t type) { name_ = name; type_ = type; }
-	type_t type() const noexcept { return type_; }
-	string name() const noexcept { return name_; }
-private:
-	string name_;
-	type_t type_;
-};
-
-class group {
-	vector<student_t> students;
-public:
-	bool operator==(const group& other);
-	group();
+	mark(student_t student, int value, task* task) {
+		student_ = student; value_ = value; task_ = task;
+	}
+	const student_t& student() const noexcept {
+		return student_;
+	}
+	int value() const noexcept {
+		return value_;
+	}
 };
 
 class stream {
-	group* arr;
 public:
-	stream();
+	stream(string name) { name_ = name; }
+	bool operator==(const stream& other) { return this->name_ == other.name_; }
+	string name() const noexcept {
+		return name_;
+	}
+	void add_group(group_t group) {
+		if (contains_group(group)) throw runtime_error("Group is already in the stream");
+		groups_.push_back(group);
+	}
+
+	void remove_group(const shared_ptr<group>& group) {
+		for (auto it = groups_.begin(); it != groups_.end(); it++) {
+			if (it->get()->name() == group->name()) {
+				groups_.erase(it);
+				return;
+			}
+		}
+		throw runtime_error("There isn't this group in the stream");
+	}
+
+	bool contains_group(const group_t& group) {
+		for (int i = 0; i < groups_.size(); i++) {
+			if (groups_[i]->name() == group->name()) {
+				return true;
+			}
+		}
+		return false;
+	}
+private:
+	vector<group_t> groups_;
+	string name_;
 };
 
 int main() {
